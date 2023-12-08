9dbc01fb1126b3b4e3e4e51af7be61b03579cb8c
mjmacam20 <arde.macam.up@phinmaed.com>
2023-12-08 15:35:42 +0800

    Second Commit

--- app/src/main/java/com/macam/marvinjohn/block5/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/macam/marvinjohn/block5/p1/quiz/MainActivity.kt
@@ -2,7 +2,7 @@ package com.macam.marvinjohn.block5.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.macam.marvin.john.block5.p1.quiz.R
+import com.macam.marvinjohn.block5.p1.quiz.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
