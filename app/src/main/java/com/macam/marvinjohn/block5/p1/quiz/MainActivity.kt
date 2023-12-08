29b7aa0194504e018ecb263e61bf4ef369917436
mjmacam20 <arde.macam.up@phinmaed.com>
2023-12-08 15:30:11 +0800

    First Commit

---
+++ app/src/main/java/com/macam/marvinjohn/block5/p1/quiz/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.macam.marvinjohn.block5.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.macam.marvin.john.block5.p1.quiz.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
